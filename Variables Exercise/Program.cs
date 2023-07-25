067d117aee2880ef30291477b4dd8d90131d53cf
Royroy205 <leroyjr2344@live.com>
2023-07-25 15:35:44 -0500

    done with redo

---
+++ Variables Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string customername = "james";
+int age = 34;
+char middlename = 'C';
+bool profilecorrect = true;
+double temp = 97.5;
+decimal weight = 175.00m;
+
+Console.WriteLine($"The customer name is {customername},His age is {age}, His middle name is {middlename}, He is a male {profilecorrect}, Today temeperature was {temp}, He weight was {weight}");
+
+
+
